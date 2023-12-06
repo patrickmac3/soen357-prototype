71d36254e607061afc46b5b530c880a54765f205
R-LS <shuyi.lin.2020@scis.smu.edu.sg>
2023-12-06 15:38:10 -0500

    fixes for gh page deployment

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import LoginPage from "./pages/LoginPage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/soen357-prototype">
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<LoginPage />} />
